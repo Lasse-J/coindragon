76251d174c4546892a7b1f431cb60c5cf8798d71
Lasse Juusela <lasse.juusela@gmail.com>
2024-06-10 21:17:00 -0300

    Initial commit

---
+++ app/components/Assets.js
@@ -0,0 +1,90 @@
+import { useState } from 'react'
+import Image from 'next/image'
+
+// Components
+import Token from './Token'
+
+// Images
+import minus from '../assets/minus.svg'
+
+const Assets = ({ tokens, setTokens }) => {
+  const [token, setToken] = useState(false)
+  const [isTokenModalOpen, setIsTokenModalOpen] = useState(false)
+
+  const tokenModalHandler = (token) => {
+    setToken(token)
+    setIsTokenModalOpen(true)
+  }
+
+  const removeHandler = (token) => {
+    if (token.length === 1) {
+      setTokens([])
+    } else {
+      // Remove from tokens
+      const index = tokens.indexOf(tokens.find((t) => t.id === token.id))
+
+      const tokensArr = tokens.slice()
+      tokensArr.splice(index, 1)
+      setTokens(tokensArr)
+    }
+  }  
+
+  return(
+    <div className="assets">
+      <h3>My Assets</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th></th>
+            <th>Token</th>
+            <th>Ticker</th>
+            <th>Price</th>
+            <th>Holdings</th>
+            <th>Total Value <small>(USD)</small></th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {tokens.map((token, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>
+                <Image
+                  src={token.market.image}
+                  width={20}
+                  height={20}
+                  alt="Token Image"
+                />
+              </td>
+              <td>{token.market.name}</td>
+              <td>{token.market.symbol.toUpperCase()}</td>
+              <td>{token.market.current_price.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</td>
+              <td>{token.balance}</td>
+              <td>{token.value.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</td>
+              <td><button onClick={() => tokenModalHandler(token)}>Details</button></td>
+              <td>
+                <button className="untrack" onClick={() => removeHandler(token)}>
+                  <Image
+                    src={minus}
+                    width={20}
+                    height={20}
+                    alt="Untrack token"
+                  />
+                </button>
+              </td>
+            </tr>  
+          ))}
+        </tbody>
+      </table>
+
+      {isTokenModalOpen &&
+        <Token setIsTokenModalOpen={setIsTokenModalOpen} token={token} />
+      }
+
+    </div>
+  )
+}
+
+export default Assets;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'assets.coingecko.com',
+				port: '',
+				pathname: '/coins/images/**',
+			},
+		],
+		domains: ['coin-images.coingecko.com'],
+	},
+}
 
 module.exports = nextConfig
